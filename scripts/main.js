663f0c954825aea3563bd34fe6cc4dcf08ddd1b9
Vera Kozlova <vera.kozlovavit@gmail.com>
2021-01-25 00:29:09 +0300

    feature: js for input field

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,28 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.popup__close');
 
-const openPopup = function() {
+function openPopup() {
   popup.classList.add('popup_opened')
 }
 
-const closePopup = function() {
-  popup.classList.remove('popup_opened')
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+const formElement = document.querySelector('.popup__container');
+let nameField = document.querySelector('.profile__name');
+let jobField = document.querySelector('.profile__occupation');
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_occupation');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameField.textContent = nameInput.value
+  jobField.textContent = jobInput.value
+  closePopup()
 }
 
 popupOpenButton.addEventListener('click', openPopup)
 popupCloseButton.addEventListener('click', closePopup)
+formElement.addEventListener('submit', formSubmitHandler);
+
