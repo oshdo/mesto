68ee47fa70ea036a1fbec21d309abbf994468405
Vera Kozlova <vera.kozlovavit@gmail.com>
2021-01-25 02:22:39 +0300

    fix: fixed js close button error

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
-const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close');
+const popup = document.querySelector('.popup')
+const popupOpenButton = document.querySelector('.profile__edit-button')
+const popupCloseButton = document.querySelector('.popup__close')
+let nameField = document.querySelector('.profile__name')
+let jobField = document.querySelector('.profile__occupation')
+let nameInput = document.querySelector('.popup__input_text_name')
+let jobInput = document.querySelector('.popup__input_text_occupation')
 
 function openPopup() {
   popup.classList.add('popup_opened')
+  nameInput.value = nameField.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
+  jobInput.value = jobField.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened')
 }
 
-const formElement = document.querySelector('.popup__container');
-let nameField = document.querySelector('.profile__name');
-let jobField = document.querySelector('.profile__occupation');
-let nameInput = document.querySelector('.popup__input_text_name');
-let jobInput = document.querySelector('.popup__input_text_occupation');
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   nameField.textContent = nameInput.value
   jobField.textContent = jobInput.value
-  closePopup()
+  popup.classList.remove('popup_opened')
 }
 
 popupOpenButton.addEventListener('click', openPopup)
 popupCloseButton.addEventListener('click', closePopup)
-formElement.addEventListener('submit', formSubmitHandler);
+popup.addEventListener('submit', formSubmitHandler)
 
